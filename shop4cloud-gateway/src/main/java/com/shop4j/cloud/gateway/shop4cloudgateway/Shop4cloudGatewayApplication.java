a4a402ef05bd262e2fd31c736a5277c503979ed1
ksying <jiace.ksying@gmail.com>
2023-03-07 23:22:18 +0800

    feat: init

---
+++ shop4cloud-gateway/src/main/java/com/shop4j/cloud/gateway/shop4cloudgateway/Shop4cloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.shop4j.cloud.gateway.shop4cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop4cloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop4cloudGatewayApplication.class, args);
+	}
+
+}
---
+++ shop4cloud-gateway/src/test/java/com/shop4j/cloud/gateway/shop4cloudgateway/Shop4cloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop4j.cloud.gateway.shop4cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop4cloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
