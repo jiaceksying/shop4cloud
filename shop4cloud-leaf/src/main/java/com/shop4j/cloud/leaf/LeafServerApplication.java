726f575b914bd592359f4bcf39f44c4d72468957
amos.wang <1833063210@qq.com>
2023-03-19 12:05:55 +0800

    feat: test consume leaf generate id

--- shop4cloud-leaf/src/main/java/com/shop4j/cloud/leaf/LeafServerApplication.java
+++ shop4cloud-leaf/src/main/java/com/shop4j/cloud/leaf/LeafServerApplication.java
@@ -1,11 +1,13 @@
 package com.shop4j.cloud.leaf;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author leaf
  */
+@EnableDubbo
 @SpringBootApplication
 public class LeafServerApplication {
 
---
+++ shop4cloud-user/src/test/java/com/shop4j/cloud/user/service/UserServiceTests.java
@@ -0,0 +1,44 @@
+package com.shop4j.cloud.user.service;
+
+import com.shop4j.cloud.api.leaf.dubbo.SegmentDubboService;
+import com.shop4j.cloud.common.response.UnifiedResponse;
+import com.shop4j.cloud.user.app.service.UserService;
+import com.shop4j.cloud.user.model.UserRegisterDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+@Slf4j
+@SpringBootTest
+class UserServiceTests {
+
+    @Autowired
+    private UserService userService;
+    @DubboReference
+    private SegmentDubboService segmentDubboService;
+
+    @Test
+    public void generateId() {
+        UnifiedResponse<Long> segmentId = segmentDubboService.getSegmentId("shop4cloud-user");
+        log.info("get segment id! result: {}", segmentId);
+    }
+
+    @Test
+    @Rollback
+    void register() {
+        UserRegisterDTO userRegisterDTO = new UserRegisterDTO();
+        userRegisterDTO.setUserId(1000L);
+        userRegisterDTO.setUserName("amos.wang");
+        userRegisterDTO.setNickName("AmosWang");
+        userRegisterDTO.setPassword("123456");
+        userRegisterDTO.setImg(null);
+        userRegisterDTO.setTempUid(null);
+
+        UnifiedResponse<Long> register = userService.register(userRegisterDTO);
+        log.info("user register! result: {}", register);
+    }
+
+}
